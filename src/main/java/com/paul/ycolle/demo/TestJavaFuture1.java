7508984ced050311a7f2f789aa9b07869dc2c3ae
paul <paulyung@outlook.com>
2017-12-26 18:22:13 +0800

    新增 Future 使用，重入锁使用

--- src/main/java/com/paul/ycolle/demo/TestJavaFuture1.java
+++ src/main/java/com/paul/ycolle/demo/TestJavaFuture1.java
@@ -1,7 +1,6 @@
 package com.paul.ycolle.demo;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.FutureTask;
+import java.util.concurrent.*;
 
 public class TestJavaFuture1 {
     public static void main(String[] args) {
@@ -15,13 +14,13 @@ public class TestJavaFuture1 {
         };
         FutureTask<String> futureTask = new FutureTask<>(callable);
         new Thread(futureTask).start();
-        synchronized (TestJavaFuture1.class) {
-            try {
-                TestJavaFuture1.class.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        String result = null;
+        try {
+            result = futureTask.get(10, TimeUnit.SECONDS);//调用此方法会阻塞线程，最多阻塞10秒（如果10秒内线程还没有执行完返回的话）
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            e.printStackTrace();
         }
+        System.out.println(result);
         System.out.println("end");
     }
 }
